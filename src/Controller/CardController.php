3dd33a4d47778106267b871692f57f061eac43a0
Study <t.kammann1614@web.de>
2018-08-09 17:42:33 +0200

    Added Todo

--- src/Controller/CardController.php
+++ src/Controller/CardController.php
@@ -3,16 +3,20 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CardController extends AbstractController {
 
+  // TODO Add a Finder, thats Find a Json File , iterate through it, save attributes to Entity, and display it in twig Template
+
   /**
    * @Route("/cards/card/{card_id}", name="app_card_info")
    */
   public function showCardInfo($card_id) {
-    return $this->render('index/index.html.twig');
+    var_dump($card_id);
+    return $this->render('card/card.html.twig');
   }
 
   /**
