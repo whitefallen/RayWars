9881fc26856ba7321b2441156c089f5f55d48dd8
Study <t.kammann1614@web.de>
2018-08-09 17:32:48 +0200

    Added Ruleset mockup

--- src/Controller/RulesetController.php
+++ src/Controller/RulesetController.php
@@ -6,9 +6,13 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-/**
- * @Route("/", name="app_index")
- */
 class RulesetController extends AbstractController {
+
+  /**
+   * @Route("/ruleset", name="app_ruleset")
+   */
+  public function show() {
+    return $this->render('ruleset/ruleset.html.twig');
+  }
 
 }
