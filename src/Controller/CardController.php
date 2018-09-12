cda4d9aa5ef756dee10621456eb2653c445e70c4
Study <t.kammann1614@web.de>
2018-09-12 23:04:50 +0200

    Fex?

--- src/Controller/CardController.php
+++ src/Controller/CardController.php
@@ -29,7 +29,7 @@ class CardController extends AbstractController {
   }
 
   /**
-   * @Route("/cardss", name="new_app_card_list")
+   * @Route("/cards", name="new_app_card_list")
    */
   public function showNewCardList() {
     $cards = $this->findAllFiles();
@@ -56,6 +56,7 @@ class CardController extends AbstractController {
     $data = $finder->files()->name('card*.json')->in($pathToCards);
     foreach($data as $date) {
         $data = json_decode($date->getContents(),true);
+        var_dump($data['id']);
         $cards[$data['id']] = $this->createCardObject($data);
     }
     return $cards;
